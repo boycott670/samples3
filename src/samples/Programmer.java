47ee5eaf93869eea1b8dc4bddeddb6ce57403e5a
Marmar Soufiane <marsouf224@gmail.com>
2018-03-02 14:19:39 +0000

    V004

---
+++ src/samples/Programmer.java
@@ -0,0 +1,21 @@
+package samples;
+
+public class Programmer {
+
+	public Programmer(String name, Class<? extends Number> lovesNumber) {
+		this.name = name;
+		this.lovesNumber = lovesNumber;
+	}
+	
+	private final String name;
+	private final Class<? extends Number> lovesNumber;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Class<? extends Number> getLovesNumber() {
+		return lovesNumber;
+	}
+	
+}

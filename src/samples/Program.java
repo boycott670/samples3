b3734a2486ec6b7212f650aa4e8946829c3f71da
marsouf <marsouf224.gmail.com>
2018-03-02 23:44:41 +0000

    V001_001

--- src/samples/Program.java
+++ src/samples/Program.java
@@ -1,14 +1,13 @@
 package samples;
 
-import samples.entities.ScrumMaster;
+import java.lang.reflect.Modifier;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		assert args != null && args.length != 0 && args [0] != null && args [0].length() != 0 && args [0].startsWith("x") && args [0].length() < 4 && new ScrumMaster(args[0], null).getLovesNumber() == null;
-
+		assert Modifier.isFinal(String.class.getModifiers());
 	}
 
 }

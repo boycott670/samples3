f7fcd04c0bac82a31cba8092d8d1201798c378a9
marsouf <marsouf224.gmail.com>
2018-03-02 19:58:24 +0000

    V005

--- src/samples/Program.java
+++ src/samples/Program.java
@@ -1,11 +1,13 @@
 package samples;
 
+import samples.entities.ScrumMaster;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		assert args != null && args.length != 0 && args [0] != null && args [0].length() != 0 && args [0].startsWith("x") && args [0].length() < 4;
+		assert args != null && args.length != 0 && args [0] != null && args [0].length() != 0 && args [0].startsWith("x") && args [0].length() < 4 && new ScrumMaster(args[0], null).getLovesNumber() == null;
 
 	}
 
---
+++ src/samples/entities/ScrumMaster.java
@@ -0,0 +1,14 @@
+package samples.entities;
+
+import samples.Programmer;
+
+public class ScrumMaster extends Programmer
+{
+
+	public ScrumMaster(String name, Class<? extends Number> lovesNumber)
+	{
+		super(name, lovesNumber);
+		// TODO Auto-generated constructor stub
+	}
+
+}

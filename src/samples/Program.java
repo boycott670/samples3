38a5cfbafe47e6078fb55cce19be3f854817135b
Marmar Soufiane <marsouf224@gmail.com>
2018-03-02 13:28:07 +0000

    V003

--- src/samples/Program.java
+++ src/samples/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		assert args != null && args.length != 0 && args [0] != null && args [0].length() != 0;
+		assert args != null && args.length != 0 && args [0] != null && args [0].length() != 0 && args [0].startsWith("x") && args [0].length() < 4;
 
 	}
 

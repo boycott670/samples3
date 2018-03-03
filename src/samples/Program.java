4dd4970c6d140a3577a1184690baa72d866bce10
marsouf <marsouf224.gmail.com>
2018-03-03 00:38:07 +0000

    V001_001_001

--- src/samples/Program.java
+++ src/samples/Program.java
@@ -6,8 +6,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		final int stringClassModifiers = String.class.getModifiers();
 		
-		assert Modifier.isFinal(String.class.getModifiers());
+		assert Modifier.isFinal(stringClassModifiers) && Modifier.isPublic(stringClassModifiers);
 	}
 
 }
